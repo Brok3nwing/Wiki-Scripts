3883ea0c32f3ca16d08043d912760a1099895bdb
Brok3nwing <108875583+Brok3nwing@users.noreply.github.com>
2022-07-15 20:25:41 +0200

    Create possible_keys.py

---
+++ possible_keys.py
@@ -0,0 +1,142 @@
+class Keys:
+    item_data = [
+        "id",  # 0
+        "name",  # 1
+        "resizeX",  # 2
+        "resizeY",  # 3
+        "resizeZ",  # 4
+        "xan2d",  # 5
+        "yan2d",  # 6
+        "zan2d",  # 7
+        "cost",  # 8
+        "isTradeable",  # 9
+        "stackable",  # 10
+        "inventoryModel",  # 11
+        "members",  # 12
+        "zoom2d",  # 13
+        "xOffset2d",  # 14
+        "yOffset2d",  # 15
+        "ambient",  # 16
+        "contrast",  # 17
+        "options",  # 18
+        "interfaceOptions",  # 19
+        "maleModel0",  # 20
+        "maleModel1",  # 21
+        "maleModel2",  # 22
+        "maleOffset",  # 23
+        "maleHeadModel",  # 24
+        "maleHeadModel2",  # 25
+        "femaleModel0",  # 26
+        "femaleModel1",  # 27
+        "femaleModel2",  # 28
+        "femaleOffset",  # 29
+        "femaleHeadModel",  # 30
+        "femaleHeadModel2",  # 31
+        "notedID",  # 32
+        "notedTemplate",  # 33
+        "team",  # 34
+        "shiftClickDropIndex",  # 35
+        "boughtId",  # 36
+        "boughtTemplateId",  # 37
+        "placeholderId",  # 38
+        "placeholderTemplateId",  # 39
+        "colorFind",  # 40
+        "colorReplace",  # 41
+        "params",  # 42
+        "countCo",  # 43
+        "countObj",  # 44
+        "textureFind",  # 45
+        "textureReplace",  # 46
+        "category"  # 47
+    ]
+
+    npcs_data = [
+        "id",
+        "name",
+        "size",
+        "chatheadModels/0",
+        "standingAnimation",
+        "rotateLeftAnimation",
+        "rotateRightAnimation",
+        "walkingAnimation",
+        "rotate180Animation",
+        "rotate90RightAnimation",
+        "rotate90LeftAnimation",
+        "actions",
+        "isMinimapVisible",
+        "combatLevel",
+        "widthScale",
+        "heightScale",
+        "hasRenderPriority",
+        "ambient",
+        "contrast",
+        "headIcon",
+        "rotationSpeed",
+        "varbitId",
+        "varpIndex",
+        "isInteractable",
+        "rotationFlag",
+        "isPet",
+        "recolorToFind",
+        "recolorToReplace",
+        "recolorToFind/5",
+        "recolorToReplace/5",
+        "configs",
+        "models",
+        "chatheadModels",
+        "retextureToFind",
+        "retextureToReplace",
+        "params"
+        "category"
+    ]
+
+
+    objects_data = [
+        "id",
+        "decorDisplacement",
+        "isHollow",
+        "name",
+        "mapAreaId",
+        "sizeX",
+        "sizeY",
+        "anInt2083",
+        "offsetX",
+        "mergeNormals",
+        "wallOrDoor",
+        "animationID",
+        "varbitID",
+        "ambient",
+        "contrast",
+        "actions",
+        "interactType",
+        "mapSceneID",
+        "blockingMask",
+        "shadow",
+        "modelSizeX",
+        "modelSizeHeight",
+        "modelSizeY",
+        "objectID",
+        "offsetHeight",
+        "offsetY",
+        "obstructsGround",
+        "contouredGround",
+        "supportsItems",
+        "configChangeDest",
+        "isRotated",
+        "varpID",
+        "ambientSoundId",
+        "aBool2111",
+        "anInt2112",
+        "anInt2113",
+        "blocksProjectile",
+        "objectModels",
+        "recolorToFind",
+        "recolorToReplace",
+        "objectTypes",
+        "retextureToFind",
+        "textureToReplace",
+        "retextureToFind",
+        "textureToReplace",
+        "anIntArray2084",
+        "params"
+    ]
