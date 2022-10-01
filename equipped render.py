8a0e7a591f2515b880260906f7e3bad749894824
Brok3nwing <108875583+Brok3nwing@users.noreply.github.com>
2022-10-01 21:40:14 +0200

    Update equipped render.py

--- equipped render.py
+++ equipped render.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
-import csv
+import numpy as np
 import pandas as pd
+import json
 
 
 #  male default kit [0, 0, 0, 0, 363, 0, 342, 356, 259, 289, 298, 270]
@@ -15,26 +16,36 @@ item_slot = []
 item_anim = []
 item_rota = []
 item_render_set = []
+cache_version = 0
 
 def read_data():
-    reader = pd.read_excel("C:/Users/Brok/Documents/Python/PycharmProjects/wiki_scripts/test.xlsx", usecols="A:F")
+    reader = pd.read_excel("C:/Users/Brok/Documents/Python/PycharmProjects/wiki_scripts/test.xlsx", usecols="A:G")
     df = pd.DataFrame(reader)
-
-    for i in range(len(df.dropna())):
-        item_id.append(int(df.id[i]))
-        item_name.append(df.name[i])
+    for i in range(len(df.id.dropna())):
+        item_id.append(int(df.id[i]) + 512)
+        item_name.append(df.fillna("").name[i])
         item_slot.append(df.slot[i])
-        item_anim.append(int(df.anim[i]) + 512)
+        item_anim.append(int(df.anim[i]))
         item_rota.append(int(df.rotation[i]))
         item_render_set.append(int(df.render_set[i]))
 
-    return item_id, item_name, item_slot, item_anim, item_rota, item_render_set
+    cache_version = df.cache_version
+    return item_id, item_name, item_slot, item_anim, item_rota, item_render_set, cache_version
+
+def get_item_name_from_cache(item_id: int):
+    version = read_data()[6][0]
+    path_to_defs = f"E:/Caches/{version}/definitions/item_defs/"
+    name = ""
+    with open(f"{path_to_defs}{item_id}.json") as f:
+        json_data = json.load(f)
+        name = json_data["name"]
+
+    return name
 
 def render_single_items():
-    rev = "208.3"
     out = "out99"
 
-    id, name, slot, anim, rota, set = read_data()
+    id, name, slot, anim, rota, set, version = read_data()
 
     male_player_colors = "0,3,15,1,0"
     female_player_colors = "7,16,26,0,0"
@@ -69,25 +80,36 @@ def render_single_items():
                      f"{str(id[i]) if slot[i] == 'jaw' else 0}"
 
         if slot[i] == "head":
-            print(male_player_kit)
-            # subprocess.call(f"java -jar E:/Renderer/renderer-all.jar --playerkit {male_player_kit} --playercolors {male_player_colors} --playerchathead --anim 589 --cache E:/Caches/{rev}/cache2 --out {out}")
-            # os.rename(f"{out}/playerchathead/[{male_player_kit.replace(',', ', ')}]_[{male_player_colors.replace(',', ', ')}].png",f"{out}/playerchathead/{name} chathead.png")
+            head_proc = f"java -jar E:/Renderer/renderer-all.jar --playerkit {male_player_kit} --playercolors {male_player_colors} --playerchathead --anim 589 --cache E:/Caches/{version[0]}/cache --out {out}"
+            subprocess.call(head_proc)
+            os.rename(f"{out}/playerchathead/[{male_player_kit.replace(',', ', ')}]_[{male_player_colors.replace(',', ', ')}].png",f"{out}/playerchathead/{name[i]} chathead.png")
 
-        # subprocess.call(f"java -jar E:/Renderer/renderer-all.jar --playerkit {male_player_kit} --playercolors {male_player_colors} --anim {anim} --yan2d {rota} --cache E:/Caches/{rev}/cache2 --out {out}")
-        # subprocess.call(f"java -jar E:/Renderer/renderer-all.jar --playerkit {female_player_kit} --playercolors {female_player_colors} --playerfemale --anim {anim} --yan2d {rota} --cache E:/Caches/{rev}/cache2 --out {out}")
-        # print(f"(female) {name[i]} : {female_player_kit}")
+
+
+        male_proc = f"java -jar E:/Renderer/renderer-all.jar --playerkit {male_player_kit} --playercolors {male_player_colors} --poseanim {anim[i]} --yan2d {rota[i]} --cache E:/Caches/{version[0]}/cache --out {out}"
+        female_proc = f"java -jar E:/Renderer/renderer-all.jar --playerkit {female_player_kit} --playercolors {female_player_colors} --playerfemale --anim {anim[i]} --yan2d {rota[i]} --cache E:/Caches/{version[0]}/cache --out {out}"
+
+        subprocess.call(male_proc)
+        subprocess.call(female_proc)
+        print(f"(female) {name[i]} : {female_player_kit}")
         print(f"(male) {name[i]} : {male_player_kit}")
-        # os.rename(f"{out}/player/[{male_player_kit.replace(',', ', ')}]_[{male_player_colors.replace(',', ', ')}].png",f"{out}/player/{name} equipped male.png")
-        # os.rename(f"{out}/player/[{female_player_kit.replace(',', ', ')}]_[{female_player_colors.replace(',', ', ')}].png", f"{out}/player/{name} equipped female.png")
-
-
+        os.rename(f"{out}/player/[{male_player_kit.replace(',', ', ')}]_[{male_player_colors.replace(',', ', ')}].png",f"{out}/player/{get_item_name_from_cache(id[i] - 512) if name[i] == '' else name[i]} equipped male.png")
+        os.rename(f"{out}/player/[{female_player_kit.replace(',', ', ')}]_[{female_player_colors.replace(',', ', ')}].png",f"{out}/player/{get_item_name_from_cache(id[i] - 512) if name[i] == '' else name[i]} equipped female.png")
 
 def render_sets():
     id, name, slot, anim, rota, set = read_data()
+    sets = {}
+    # sets = {int(set[0]) : [name[0]]}
 
+    x = {int(set[0]) : [name[0]]}
+    y = {int(set[1]): [name[1]]}
+    sets.update(x)
+    sets.update(y)
 
-    for i in range(len(set)):
-
+    print(sets)
+    # for i in range(len(set)):
+    #     if set[i] > 0:
+    #         sets[] = [name[i]]
 
     # male_player_kit = f"{str(id[i]) if slot[i] == 'head' else 0}," \
     #                   f"{str(id[i]) if slot[i] == 'cape' else 0}," \
@@ -104,7 +126,7 @@ def render_sets():
 
     # if set.count(1) > 1:
     #     subprocess.call(
-    #         f"java -jar E:/Renderer/renderer-all.jar --playerkit  --playercolors {male_player_colors} --anim {anim} --yan2d {rota} --cache E:/Caches/{rev}/cache2 --out {out}")
+    #         f"java -jar E:/Renderer/renderer-all.jar --playerkit  --playercolors {male_player_colors} --anim {anim} --yan2d {rota} --cache E:/Caches/{version[0]}/cache2 --out {out}")
 
 
-render_sets()
+render_single_items()
